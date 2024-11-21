a8668b7f9c17c560547a11c2db315a6fe5aa2282
Threel1ne <124362574+Threel1ne@users.noreply.github.com>
2024-11-21 13:23:08 +0700

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import LazyLoad from "react-lazyload";
+import { motion, useScroll, useSpring } from "framer-motion";
+
 function MyButton() {
   return (
 
@@ -256,4 +260,36 @@ var modal = document.getElementById("myModal");
 }
 
     
+export default function Index() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+  const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  console.log(isOpen);
+  return (
+    <>
+      <LazyLoad hedight={200}>
+        <motion.div
+          className="progress-bar fixed top-0 left-0 h-1 md:h-2 right-0 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% z-50"
+          style={{ scaleX: scaleX }}
+        />
+        <div
+          className={`flex flex-col gap-16 items-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
+        >
+          <Navbar isOpen={isOpen} toggleMenu={toggleMenu} />
+          <Hero />
+          <EveryLink />
+          <ImageSlideRevealGrid />
+        </div>
+
+        <Contact />
+      </LazyLoad>
+    </>
+  );
+}
